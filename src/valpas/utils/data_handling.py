c8b9668b01dfb5ca09e90a6a547f69e312b39932
Yannick Mahlich <yannick.mahlich@pnnl.gov>
2024-09-09 13:23:08 -0700

    fixed binning to actually return a binned DataFrame

--- src/valpas/utils/data_handling.py
+++ src/valpas/utils/data_handling.py
@@ -46,8 +46,8 @@ def prep_data(file_handle: str, cut: bool=False, threshold: bool=False,
 
     return df.transpose()
 
-def bin(df: pd.DataFrame, num_bins: int=100) -> pd.DataFrame:
-    df.apply(lambda x: pd.cut(x, bins=num_bins), axis=0)
+def bin(df: pd.DataFrame, num_bins: int=2) -> pd.DataFrame:
+    df = df.apply(lambda x: pd.cut(x, bins=num_bins), axis=0)
     return df
 
 def threshold_df(df: pd.DataFrame) -> pd.DataFrame:
