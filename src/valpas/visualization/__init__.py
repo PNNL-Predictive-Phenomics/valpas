af596712e598b6d9423cbba2da5d7938ff373f56
Jason McDermott <Jason.McDermott@pnnl.gov>
2025-11-15 07:02:09 -0800

    Added graph visualization for Jupyter notebook

--- src/valpas/visualization/__init__.py
+++ src/valpas/visualization/__init__.py
@@ -5,5 +5,6 @@ All functionality regarding visualization is gathered here.
 __all__ = [
     'heatmap',
     'ppv_comparison_heatmap',
-    'edge_venn_diagram_plot'
+    'edge_venn_diagram_plot',
+    'graph_visualization'
     ]
